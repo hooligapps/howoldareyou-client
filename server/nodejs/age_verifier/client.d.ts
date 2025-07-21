4b90b2ea7d35f4e6704408e2b5b4121052cd2df5
emtyweb <EmtyWeb@gmail.com>
2025-07-21 13:16:02 +0200

    reformat

--- server/nodejs/age_verifier/client.d.ts
+++ server/nodejs/age_verifier/client.d.ts
@@ -1,12 +1,17 @@
 // client.d.ts
 export class AgeVerifierClient {
-  constructor(apiUrl: string, appId: string, apiKey: string,  timeout?: number);
-  getSignature(data: {[key: string]: any}): string;
-  needVerification(clientIp: string, userId: string): Promise<{ result: number }>;
-  startCheckAgeVerification(sessionId: string, clientIp: string, userId?: string): Promise<{
-    result: number,
-    url?: string
-  }>;
-  checkAgeVerificationResult(sessionId: string): Promise<{ result: number }>;
-  updateVerificationResult(sessionId: string, userId: string);
+    constructor(apiUrl: string, appId: string, apiKey: string, timeout?: number);
+
+    getSignature(data: { [key: string]: any }): string;
+
+    needVerification(clientIp: string, userId: string): Promise<{ result: number }>;
+
+    startCheckAgeVerification(sessionId: string, clientIp: string, userId?: string): Promise<{
+        result: number,
+        url?: string
+    }>;
+
+    checkAgeVerificationResult(sessionId: string): Promise<{ result: number }>;
+
+    updateVerificationResult(sessionId: string, userId: string): Promise<void>;
 }
